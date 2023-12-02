22d3ec1551396c29d9b3a566de344ad7dd7c56f0
Ankit Patel <ankit.patel@easebuzz.in>
2023-12-02 15:26:35 +0530

    Removed ide files and updated code-base

--- helpers/kite_connect.py
+++ helpers/kite_connect.py
@@ -1,13 +1,32 @@
+import requests
 from kiteconnect import KiteConnect
 from flask import render_template
 
-class KiteConnectHelper:
+class RequestHelper:
+
+    KITE_TOKEN_URL = "https://api.kite.trade/session/token"
+
+    def request_for_token(self, data):
+        """
+        The function `request_for_token` sends a request to obtain an access token and updates the data
+        with the response if the status is 200, then returns a session object.
+        :return: the result of the `kite_helper._session(data)` function call.
+        """
+        access_token = request.post(KITE_TOKEN_URL, data={
+            'api_key': env_config['APP_TOKEN'],
+            'request_token': data['request_token']
+        })
+        if access_token.status == 200:
+            data.update(json.loads(access_token))
+        return data        
+
+class KiteConnectHelper(RequestHelper):
 
     def __init__(self, api_key) -> None:
         self.api_key = api_key
         self.kite_connect = KiteConnect(api_key)
     
-    def _connect(self, ):
+    def _connect(self, ) :
         """
         Redirect the user to the login url obtained
         from kite.login_url(), and receive the request_token
@@ -17,7 +36,7 @@ class KiteConnectHelper:
         """
         #Sample URL https://kite.zerodha.com/connect/login?v=3&api_key=xxx&redirect_params=some%3DX%26more%3DY
         # login_url = self.kite_connect.login_url()
-        login_url = f"https://kite.zerodha.com/connect/login?v={self.api_key}"
+        login_url = f"https://kite.zerodha.com/connect/login?api_key={self.api_key}"
         return render_template('connect.html', context={'url': login_url })
         
     def _session(self, data):
@@ -31,5 +50,6 @@ class KiteConnectHelper:
         :return: the `kite` object.
         """
         data = self.kite_connect.generate_session(data["request_token"], api_secret=data["api_secret"])
+        self.request_for_token(data["request_token"])
         self.kite_connect.set_access_token(data["access_token"])
         return self.kite_connect
--- main.py
+++ main.py
@@ -12,12 +12,26 @@ kite_helper = None
 
 @app.route("/connect/login/")
 def login():
-    kite_helper = KiteConnectHelper(env_config['APP_TOKEN'])
+    """
+    The login function creates an instance of KiteConnectHelper and connects to the API using the
+    provided API key.
+    :return: The login function is returning the result of the `_connect()` method of the `kite_helper`
+    object.
+    """
+    kite_helper = KiteConnectHelper(env_config['API_KEY'])
     return kite_helper._connect()
-    # return KiteConnectHelper(env_config['APP_TOKEN'])._connect()
 
-@app.route("/connect/login/callback/<data>")
+@app.route("/connect/login/request/<data>")
 def login_callback(data):
+    """
+    The function `login_callback` checks if `kite_helper` is available and calls its `_session` method
+    with the provided `data`, otherwise it returns an error message.
+    
+    :param data: The `data` parameter is the input data that is passed to the `login_callback` function.
+    It is used as an argument when calling the `_session` method of the `kite_helper` object
+    :return: a dictionary with two key-value pairs. The "message" key has the value "Exception
+    occurred::Kite connect not found" and the "status" key has the value False.
+    """
     if kite_helper:
         return kite_helper._session(data)
     return {"message": "Exception occurred::Kite connect not found", "status": False }
